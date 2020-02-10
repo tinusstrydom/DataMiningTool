cf76a611f03d91031c0cbe3cebb6319b9ea33fcf
Tinus Strydom <tinuss@loans-inc.co.za>
2020-02-10 22:11:09 +0200

    added data importing

--- dataMiningTool.py
+++ dataMiningTool.py
@@ -1,2 +1,22 @@
 # Tinus Strydom, 2019/08/20
 # Data Mining program removing all useful and unknown information
+
+#imports modules/packages
+import pandas as pd
+import numpy as np
+
+#functions
+def dataSet():
+    dataSet = pd.read_csv("winequality-red.csv")
+    data, target = dataSet[['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']], dataSet[['quality']]
+    return data, target
+
+
+
+#main function
+def main():
+    data, target = dataSet()
+    print(data, target)
+
+if __name__ == "__main__" :
+    main()
