0dd7d97d6359ffb3b400d51030063e899c1cf1a7
Tinus Strydom <tinuss@loans-inc.co.za>
2020-04-27 09:02:19 +0200

    added histogram visualization

--- dataMiningTool.py
+++ dataMiningTool.py
@@ -4,66 +4,61 @@
 #imports modules/packages
 import pandas as pd
 import numpy as np
-import pylab as pl
-
-
+from pylab import plot,figure,subplot,hist,xlim,show,xlabel,ylabel,title 
 
 #functions
 def overviewdata():
     dataSet = pd.read_csv("wine.csv", delimiter=';')
     data, target = dataSet[['fixed_acidity','volatile_acidity','citric_acid','residual_sugar','chlorides','free_sulfur_dioxide','total_sulfur_dioxide','density','pH','sulphates','alcohol']],dataSet[['quality']]
-    return data, target, dataSet.info()
+    qltylist = []
+    for i in target.quality:
+        if not i in qltylist:
+            qltylist.append(i)
+            qltylist.sort()
+    return data, target, dataSet.info(), qltylist
 
- 
-def plotdata(data, target):
+def plotdata(data, target, qltylist):
     #print(data.fixed_acidity.loc[target.quality == 3])
-    pl.xlabel('Free Sulfur Dioxide')
-    pl.ylabel('Total Sulfur Dioxide')
-    pl.title('Relation of sulfur dioxide on the quality')
-    pl.plot(data.free_sulfur_dioxide.loc[target.quality == 3],data.total_sulfur_dioxide.loc[target.quality == 3],'ro')
-    pl.plot(data.free_sulfur_dioxide.loc[target.quality == 4],data.total_sulfur_dioxide.loc[target.quality == 4],'go')
-    pl.plot(data.free_sulfur_dioxide.loc[target.quality == 5],data.total_sulfur_dioxide.loc[target.quality == 5],'bo')
-    pl.plot(data.free_sulfur_dioxide.loc[target.quality == 6],data.total_sulfur_dioxide.loc[target.quality == 6],'mo')
-    pl.plot(data.free_sulfur_dioxide.loc[target.quality == 7],data.total_sulfur_dioxide.loc[target.quality == 7],'co')
-    pl.plot(data.free_sulfur_dioxide.loc[target.quality == 8],data.total_sulfur_dioxide.loc[target.quality == 8],'ko')
-    pl.show()
+    xlabel('Free Sulfur Dioxide')
+    ylabel('Total Sulfur Dioxide')
+    title('Relation of sulfur dioxide on the quality')
+    color = ['ro','go','bo','mo','co','ko','yo']
+    colorIt = iter(color)
+    for i in qltylist:
+        plot(data.free_sulfur_dioxide.loc[target.quality == i],data.total_sulfur_dioxide.loc[target.quality == i],next(colorIt))
+    show()
 
-def histodata(data, target):
-    #use the alcohol column to see 
+def histodata(data, target, qltylist):
     xmin = min(data.alcohol)
     xmax = max(data.alcohol)
-    pl.figure()
-    pl.subplot(611)
-    pl.hist(data.alcohol.loc[target.quality==3],color='r',alpha=.7)
-    pl.xlim(xmin,xmax)
-    pl.subplot(612)
-    pl.hist(data.alcohol.loc[target.quality==4],color='g',alpha=.7)
-    pl.xlim(xmin,xmax)
-    pl.subplot(613)
-    pl.hist(data.alcohol.loc[target.quality==5],color='b',alpha=.7)
-    pl.xlim(xmin,xmax)
-    pl.subplot(614)
-    pl.hist(data.alcohol.loc[target.quality==6],color='m',alpha=.7)
-    pl.xlim(xmin,xmax)
-    pl.subplot(615)
-    pl.hist(data.alcohol.loc[target.quality==7],color='c',alpha=.7)
-    pl.xlim(xmin,xmax)
-    pl.subplot(616)
-    pl.hist(data.alcohol.loc[target.quality==8],color='k',alpha=.7)
-    pl.xlim(xmin,xmax)
-    pl.show()
-
+    count = 421
+    color = ['r','g','b','m','c','k','y']
+    colorIt = iter(color)
+    
+    figure(figsize=(10,10),tight_layout=True)
+    
+    for i in qltylist:
+        subplot(count)
+        title('Relationship of alcohol and the quality of wine')
+        hist(data.alcohol.loc[target.quality==i],color=next(colorIt),alpha=.7,)
+        title('Quality = '+str(i))
+        xlabel("Alcohol")
+        ylabel("Quality")
+        xlim(xmin,xmax)
+        count+=1  
+    show()
+    
 #main function
 def main():
     print('Welcome to data mining tool!')
     print('Lets see an overview of the data\n')
     #View overview of data
-    data, target, dataSet = overviewdata()
-
+    data, target, dataSet, qltylist = overviewdata()
     #Visualization
-    print('\nLets plot and visualize the data')
-    plotdata(data, target)
-    histodata(data, target)
+    print('\nPlotting relation of sulfur dioxide on the quality')
+    plotdata(data, target, qltylist)
+    print('\nPlot histogram of alcohol on the quality')
+    histodata(data, target, qltylist)
 
     #Classify and cluster
 
